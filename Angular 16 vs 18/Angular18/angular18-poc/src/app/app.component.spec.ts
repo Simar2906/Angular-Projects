4df59e39bdccc2319f9c304d37cc203b63963d00
Simar2906 <simar2906@gmail.com>
2024-09-19 11:24:32 +0530

    Re-made Projects

--- Angular 16 vs 18/Angular18/angular18-poc/src/app/app.component.spec.ts
+++ Angular 16 vs 18/Angular18/angular18-poc/src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular16-poc'`, () => {
+  it(`should have the 'angular18-poc' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular16-poc');
+    expect(app.title).toEqual('angular18-poc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular16-poc app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular18-poc');
   });
 });
